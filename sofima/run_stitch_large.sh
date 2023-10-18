19927326ea8a060cd1f0e6e62dd03a1ced7b4734
David Turner <dmturner@princeton.edu>
2023-10-18 14:25:38 -0400

    Add a slurm script for running large stiching.

---
+++ sofima/run_stitch_large.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -t 8:00:00
+#SBATCH -N 1
+#SBATCH -n 32
+#SBATCH --mem=400GB
+#SBATCH --gres=gpu:4
+#SBATCH --constraint=gpu80
+
+module load cudatoolkit/12.2
+module load anaconda3/2023.3 
+conda activate stiching
+
+export PYTHONUNBUFFERED=TRUE
+
+python -u stitch.py Part1_reel1068_blade1_20230727/bladeseq-2023.08.01-19.14.39/s1257-2023.08.01-19.14.39 --output_dir runs/test_run_large --no_render
